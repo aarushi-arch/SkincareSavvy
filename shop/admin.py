e479c59da4b149553818741a68c151626363d639
Aarushi Sharma <sharmaaarushi582@gmail.com>
2026-01-08 10:03:13 +0545

    fixed the "add to shelf" feature

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Cart, CartItem
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,37 @@
 from django.db import models
+from django.conf import settings
+from recommendations.models import Product
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    brand = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
-    description = models.TextField()
 
-    suitable_skin_types = models.JSONField()
-    targets_concerns = models.JSONField()
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="cart"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def total_price(self):
+        return sum(item.subtotal for item in self.items.all())
+
+    def __str__(self):
+        return f"Cart of {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def subtotal(self):
+        return (self.product.price or 0) * self.quantity
 
     def __str__(self):
         return self.name
